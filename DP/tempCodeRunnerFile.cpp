08a419c6720ce8bfe1019ea66b8bd99573665c46
Nilesh1206 <nileshgopale98@gmail.com.com>
2022-03-16 07:59:13 +0530

    updated

--- DP/tempCodeRunnerFile.cpp
+++ DP/tempCodeRunnerFile.cpp
@@ -1,54 +1,132 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// void swap_three(int*, int*,int*);
+
+// int main()
+// {
+
+//     // write your code here
+
+//     cout<<"Enter three numbers: "<<endl;
+//     // three numbers input taken by the user
+//     int p1,p2,p3;
+//     cout<<"var1: ";
+//     cin>>p1;
+//     cout<<"var2: ";
+//     cin>>p2;
+//     cout<<"var3: ";
+//     cin>>p3;
+
+//     // Number before swapping
+//     cout<<"\nNumbers before swapping: "<<endl;
+//     cout<<"var1: "<<p1<<", var2:"<<p2<<", var3:"<<p3<<endl;
+
+    
+
+//     // Utility function , which will swaps three numbers
+//     // Here we are passing the numbers by reference
+//     // means we are passing the memory addresses of values
+//     swap_three(&p1,&p2,&p3);
+    
+//     // Numbers after swapping
+//     cout<<"\nNumbers after swapping: "<<endl;
+//     cout<<"var1: "<<p1<<", var2:"<<p2<<", var3:"<<p3<<endl;
+// }
+
+
+// // Utility function which swaps the numbes
+// // This function takes the parameters in the memory referece
+// void swap_three(int* p1,int* p2,int *p3)
+// {
+//     // write your code here
+
+//     // here we are swapping the first and second number
+//     int temp=*p2;
+//     *p2=*p1;
+//     *p1=temp;
+
+
+//     // Here we are swapping the first and third number
+//     temp=*p1;
+//     *p1=*p3;
+//     *p3=temp;
+// }
+
+
+#include <bits/stdc++.h>
+#define fast()                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define int long long
 using namespace std;
+#define endl '\n'
+const int mod=1e9+7;
 
-void swap_three(int*, int*,int*);
 
-int main()
+void DFS(int src,vector<int>adj[],vector<bool>&visited,int &ct,int &danger)
 {
+    if(visited[src]) return;
+    visited[src]=true;
+    ct+=1;
+    if(ct>1)
+    {
+        danger*=2;
+    }
+    for(auto it:adj[src])
+    {
+        DFS(it,adj,visited,ct,danger);
+    }
+}
 
-    // write your code here
+void solve()
+{
+    int n;
+    cin>>n;
+    int low=1;
+    int high=1e9;
+    int ans=-1;
+    while(low<high)
+    {
+        int mid=low+(high-low)/2;
+        int sum=(mid*mid);
+        int temp=mid;
+        int numSum=0;
+        while(temp>0)
+        {
+            numSum+=temp%10;
+            temp/=10;
+        }
+        sum+=numSum;
+        if(sum>=n)
+        {
+            ans=mid;
+            high=mid-1;
+        }
+        else
+        {
+            low=mid;
+        }
+    }
+    cout<<ans<<endl;
 
-    cout<<"Enter three numbers: "<<endl;
-    // three numbers input taken by the user
-    int p1,p2,p3;
-    cout<<"var1: ";
-    cin>>p1;
-    cout<<"var2: ";
-    cin>>p2;
-    cout<<"var3: ";
-    cin>>p3;
-
-    // Number before swapping
-    cout<<"\nNumbers before swapping: "<<endl;
-    cout<<"var1: "<<p1<<", var2:"<<p2<<", var3:"<<p3<<endl;
-
-    
-
-    // Utility function , which will swaps three numbers
-    // Here we are passing the numbers by reference
-    // means we are passing the memory addresses of values
-    swap_three(&p1,&p2,&p3);
-    
-    // Numbers after swapping
-    cout<<"\nNumbers after swapping: "<<endl;
-    cout<<"var1: "<<p1<<", var2:"<<p2<<", var3:"<<p3<<endl;
 }
 
 
-// Utility function which swaps the numbes
-// This function takes the parameters in the memory referece
-void swap_three(int* p1,int* p2,int *p3)
+signed main()
 {
-    // write your code here
-
-    // here we are swapping the first and second number
-    int temp=*p2;
-    *p2=*p1;
-    *p1=temp;
-
-
-    // Here we are swapping the first and third number
-    temp=*p1;
-    *p1=*p3;
-    *p3=temp;
+// #ifndef ONLINE_JUDGE
+//         freopen("inputf.in", "r", stdin);
+//         freopen("outputf.out", "w", stdout);
+// #endif
+    fast();
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     solve();
+    // }
+    solve();
+    return 0;
 }
---
+++ Daily_Coding_problems/88_Invert_Binary_Tree.cpp
@@ -0,0 +1,77 @@
+/*
+This problem was asked by Google.
+
+Invert a binary tree.
+
+For example, given the following tree:
+
+    a
+   / \
+  b   c
+ / \  /
+d   e f
+should become:
+
+  a
+ / \
+ c  b
+ \  / \
+  f e  d
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Tree
+{
+    int val;
+    Tree *left;
+    Tree *right;
+    Tree(int data)
+    {
+        val=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+Tree* InvertBinaryTree(Tree* root)
+{
+    if(!root)
+    {
+        return root;
+    }
+    Tree* temp=new Tree(root->val);
+    temp->right=InvertBinaryTree(root->left);
+    temp->left=InvertBinaryTree(root->right);
+    return temp;
+}
+
+
+
+void Inorder(Tree* root)
+{
+    if(!root)
+    {
+        return;
+    }
+    Inorder(root->left);
+    cout<<root->val<<" ";
+    Inorder(root->right);
+}
+
+int main()
+{
+    Tree *root=new Tree(1);
+    root->left= new Tree(2);
+    root->right=new Tree(3);
+    root->left->left=new Tree(4);
+    root->left->right=new Tree(5);
+    root->right->left=new Tree(6);
+    root->right->right=new Tree(7);
+    Inorder(root);
+    Tree* newRoot=InvertBinaryTree(root);
+    cout<<endl;
+    Inorder(newRoot);
+    return 0;
+}
